591dc53372e7f4708a9ce44174786fa3d0fa6ca8
Philip Guo <philip@pgbovine.net>
2016-05-07 22:36:37 +0000

    got script working independent of directories

--- run-cpp-backend.sh
+++ run-cpp-backend.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DN=`dirname $0`
+
 # $1 is a string representing the C/C++ program to be run within Docker
 # $2 is either c or cpp, depending on whether to compile C or C++
 
@@ -16,10 +18,10 @@ fi
 # WITHOUT interpreting escape chars such as '\n' ...
 # echo doesn't work here since it interprets '\n' and other chars
 #
-cat <<ENDEND > $FN
+cat <<ENDEND > $DN/$FN
 $1
 ENDEND
 
-$CC -ggdb -O0 -fno-omit-frame-pointer -o usercode.exe $FN
-valgrind-3.11.0/inst/bin/valgrind --tool=memcheck --source-filename=$FN --trace-filename=usercode.vgtrace ./usercode.exe
-python vg_to_opt_trace.py --create_jsvar=trace usercode > usercode.trace
+$CC -ggdb -O0 -fno-omit-frame-pointer -o $DN/usercode.exe $DN/$FN
+$DN/valgrind-3.11.0/inst/bin/valgrind --tool=memcheck --source-filename=$DN/$FN --trace-filename=$DN/usercode.vgtrace $DN/usercode.exe
+python $DN/vg_to_opt_trace.py --create_jsvar=trace $DN/usercode > $DN/usercode.trace
