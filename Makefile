a2e0630f90956ea841e850513f40eaaf3f638bb3
Philip Guo <philip@pgbovine.net>
2016-05-08 04:59:45 +0000

    bam

--- Makefile
+++ Makefile
@@ -8,4 +8,7 @@ dockerbash:
 	docker run -t -i --rm --user=netuser --net=none --cap-drop all pgbovine/opt-cpp-backend:v1 bash
 
 test:
+	docker run -t -i --rm --user=netuser --net=none --cap-drop all pgbovine/opt-cpp-backend:v1 /tmp/opt-cpp-backend/run-cpp-backend.sh "int main() {int x=12345;}" c
+
+regtest:
 	docker run -t -i --rm --user=netuser --net=none --cap-drop all pgbovine/opt-cpp-backend:v1 /bin/sh -c 'cd /tmp/opt-cpp-backend/tests && python golden_test.py --all'
--- run-cpp-backend.sh
+++ run-cpp-backend.sh
@@ -14,6 +14,8 @@ else
   FN="usercode.cpp"
 fi
 
+rm -f usercode.* # in case there are stray files in the container
+
 # tricky! use a heredoc to pipe the $1 argument into the stdin of cat
 # WITHOUT interpreting escape chars such as '\n' ...
 # echo doesn't work here since it interprets '\n' and other chars
@@ -22,7 +24,13 @@ cat <<ENDEND > $DN/$FN
 $1
 ENDEND
 
-$CC -ggdb -O0 -fno-omit-frame-pointer -o $DN/usercode.exe $DN/$FN
-# tricky! --source-filename takes a basename only, not a dirname:
-$DN/valgrind-3.11.0/inst/bin/valgrind --tool=memcheck --source-filename=$FN --trace-filename=$DN/usercode.vgtrace $DN/usercode.exe
-python $DN/vg_to_opt_trace.py --jsondump $DN/usercode
+$CC -ggdb -O0 -fno-omit-frame-pointer -o $DN/usercode.exe $DN/$FN 2> $DN/gcc.errs
+if [ $? -eq 0 ]
+then
+  # tricky! --source-filename takes a basename only, not a dirname:
+  $DN/valgrind-3.11.0/inst/bin/valgrind --tool=memcheck --source-filename=$FN --trace-filename=$DN/usercode.vgtrace $DN/usercode.exe > /dev/null # silence stdout
+  python $DN/vg_to_opt_trace.py --jsondump $DN/usercode
+else
+  # TODO: report compiler errors gracefully here using $DN/gcc.errs
+  echo '{"code": "", trace :[{"event":"uncaught_exception","exception_msg":"COMPILER ERROR, UGH!"}]}';
+fi
